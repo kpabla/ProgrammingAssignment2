7349390966173bce7c52b798755f962abe704ea9
Kuldip Pabla <kuldeep@obssa.com>
2014-06-22 09:24:15 -0700

    Assignment for cached inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assignment for chaching matrix
 
-## Write a short comment describing this function
+# makeCacheMatrix : creates a vector (list) of functions
+## set the value the matrix
+## get the value the matrix
+## set the inverse matrix using solve()
+## get the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
 
+    get <- function() x
+
+    setmatrix <- function(solve) m <<- solve
+
+    getmatrix <- function() m
+
+    list(set=set, get=get,
+        setmatrix = setmatrix,
+        getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+# chacheSolve computes inverse of the special matrix returned by makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if (!is.null(m)) {
+        message ("getting cached inverse of matrix")
+        return (m)
+    }
+    matrix <- x$get()
+
+    ## it will be good to check if det(matrix) is zero or not
+
+    m <- solve (matrix, ...)
+    x$setmatrix (m)
+    m
 }
